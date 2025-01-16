c6923b9a6a7f26dc1b3c55ce3710710da6cbc073
Margaretha Naibaho <margarethanaibaho2@gmail.com>
2025-01-16 15:48:45 +0700

    set custom grey color

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,4 +4,9 @@ class CustomColors{
   static const Color primaryGreen = Color.fromRGBO(98, 206, 60, 1);
   static const Color darkGreen = Color.fromARGB(255, 68, 151, 40);
   static const Color lightGreen = Color.fromARGB(255, 155, 216, 314);
+
+  static const Color primaryGrey = Color.fromARGB(255, 115, 115, 115);
+  static const Color lightGrey = Color.fromARGB(255, 185, 185, 185);
+  static const Color cardGrey = Color.fromARGB(255, 238, 238, 238);
+  static const Color searchGrey = Color.fromARGB(255, 195, 191, 191);
 }
