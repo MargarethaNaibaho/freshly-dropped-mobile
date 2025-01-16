95333251cba699ff8a1521f1cac65f4ff01af162
Margaretha Naibaho <margarethanaibaho2@gmail.com>
2025-01-16 15:40:44 +0700

    set custom green color

---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CustomColors{
+  static const Color primaryGreen = Color.fromRGBO(98, 206, 60, 1);
+  static const Color darkGreen = Color.fromARGB(255, 68, 151, 40);
+  static const Color lightGreen = Color.fromARGB(255, 155, 216, 314);
+}
