d9e95733801bdf2d4ddb99a9b65fba1321167e0f
Margaretha Naibaho <margarethanaibaho2@gmail.com>
2025-01-18 17:07:39 +0700

    refactor appbar with green background and search feature

--- lib/screens/recipes_screen.dart
+++ lib/screens/recipes_screen.dart
@@ -1,6 +1,6 @@
-import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:freshy_food/styles/colors.dart';
+import 'package:freshy_food/base/botton_nav.dart';
+import 'package:freshy_food/widgets/app_bar_green.dart';
 import 'package:freshy_food/widgets/single_card_half_recipe.dart';
 
 class RecipesScreen extends StatefulWidget {
@@ -50,74 +50,13 @@ class _RecipesScreenState extends State<RecipesScreen>{
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: CustomColors.lightGreen,
-        toolbarHeight: 86,
-        title: Container(
-          padding: const EdgeInsets.symmetric(vertical: 21),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              GestureDetector(onTap: (){}, 
-                child: Icon(
-                  Icons.keyboard_arrow_left_rounded,
-                  size: 40,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(width: 20,),
-              Expanded(
-                child: Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 12),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                        child: TextField(
-                          focusNode: focusNode,
-                          style: TextStyle(
-                            color: focusNode.hasFocus ? Colors.black : CustomColors.searchGrey,
-                            fontWeight: FontWeight.w300,
-                          ),
-                          decoration: InputDecoration(
-                            isDense: true,
-                            contentPadding: EdgeInsets.zero,
-                            hintText: 'Pesto Pasta',
-                            hintStyle: TextStyle(
-                              color: focusNode.hasFocus ? Colors.black : CustomColors.searchGrey,
-                              fontWeight: FontWeight.w300,
-                            ),
-                            border: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                      IconButton(onPressed: (){}, 
-                        icon: Icon(
-                          FluentSystemIcons.ic_fluent_search_regular,
-                          size: 27,
-                          color: CustomColors.searchGrey,
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ),
-              const SizedBox(width: 20,),
-              GestureDetector(onTap: (){}, 
-                child: Icon(
-                  Icons.filter_alt_outlined,
-                  size: 40,
-                  color: Colors.white,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      appBar: AppBarGreen(linkBack: (context){
+        Navigator.pushAndRemoveUntil(
+            context, 
+            MaterialPageRoute(builder: (context) => BottomNavBar()),
+            (Route<dynamic> route) => false,
+          );
+      }, linkRight: (){}, focusNode: focusNode),
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
         children: [
---
+++ lib/widgets/app_bar_green.dart
@@ -0,0 +1,84 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
+import 'package:flutter/material.dart';
+import 'package:freshy_food/styles/colors.dart';
+
+class AppBarGreen extends StatelessWidget implements PreferredSizeWidget{
+  const AppBarGreen({super.key, required this.linkBack, required this.linkRight, required this.focusNode, this.isCart = false});
+  final void Function(BuildContext) linkBack;
+  final void Function() linkRight;
+  final bool isCart;
+
+  final FocusNode focusNode;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: CustomColors.lightGreen,
+      toolbarHeight: 86,
+      title: Container(
+        padding: const EdgeInsets.symmetric(vertical: 21),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            GestureDetector(onTap: () => linkBack(context), 
+              child: Icon(
+                Icons.keyboard_arrow_left_rounded,
+                size: 40,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(width: 20,),
+            Expanded(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 11  , vertical:11),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Flexible(
+                      child: TextField(
+                        focusNode: focusNode,
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w300,
+                        ),
+                        decoration: InputDecoration(
+                          isDense: true,
+                          contentPadding: EdgeInsets.zero,
+                          hintText: 'Pesto Pasta',
+                          hintStyle: TextStyle(
+                            color: focusNode.hasFocus ? Colors.black : CustomColors.searchGrey,
+                            fontWeight: FontWeight.w300,
+                          ),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    Icon(FluentSystemIcons.ic_fluent_search_regular,
+                      size: 27,
+                      color: CustomColors.searchGrey,
+                    ),
+                  ],
+                ),
+              )
+            ),
+            const SizedBox(width: 20,),
+            GestureDetector(onTap: linkRight, 
+              child: Icon(
+                isCart ? Icons.shopping_cart_outlined : Icons.filter_alt_outlined,
+                size: 40,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(86);
+}
